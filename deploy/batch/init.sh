c2855ec345fde897d953b5b098e71070a25cf82a
Roman Rybalko <devel@romanr.info>
2015-12-05 01:57:53 +0300

    p2: proxy ru

---
+++ deploy/batch/init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -ex
+. ./framework.sh
+
+for p in hosts_batch/*.pw; do [ -e $p ]; done
+deploy_batch 020_deploy
+
+for p in hosts_batch/*.pw; do rm -vf $p; done
+deploy_batch 030_secure_home
