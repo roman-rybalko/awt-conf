620c0b2c1f5fcfeb21816079d0387c209cb97b72
Roman Rybalko <devel@romanr.info>
2015-12-07 17:52:00 +0300

    scripts: init jessie for berihost

---
+++ scripts/init-jessie-beri/rc.sh
@@ -0,0 +1,9 @@
+h=XX.hosts.advancedwebtesting.net
+echo $h > /etc/hostname
+hostname $h
+
+echo "nameserver 77.88.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
+service postfix restart || true
+service squid3 restart || true
