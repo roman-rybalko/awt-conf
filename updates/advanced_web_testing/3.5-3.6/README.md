8948b21375606a6ae49a8869a8016c3dee91392a
Roman Rybalko <devel@romanr.info>
2015-12-28 15:57:20 +0300

    awt 3.6 upd2

--- updates/advanced_web_testing/3.5-3.6/README.md
+++ updates/advanced_web_testing/3.5-3.6/README.md
@@ -2,6 +2,5 @@ awt 3.6 update
 --------------
 
 1. awt
-2. error.php -> ui/error.php
 3. demo.php -> ui/demo.php
 4. ui
--- updates/advanced_web_testing/3.5-3.6/error.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once '../web_construction_set/autoload.php';
-$error = new \AdvancedWebTesting\Error();
-$error->run();
