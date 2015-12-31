45122c944eb8398d99a626a4f9283c89217e8642
Roman Rybalko <devel@romanr.info>
2015-12-31 20:25:38 +0300

    deploy: munin backup/restore fix

--- deploy/batch/munin_restore.sh
+++ deploy/batch/munin_restore.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -ex
 . ./framework.sh
+export RSYNC_USER=awt-repl
+export RSYNC_PASSWORD=ziZianQTGxlDBfZy4LdG9s
 deploy_batch 442_munin_restore
--- deploy/tasks/441_munin_backup/remote.sh
+++ deploy/tasks/441_munin_backup/remote.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -ex
 . ./remote.conf
 
-cd /usr/lib/
-tar -cf /var/backups/munin.tar munin
-chmod -v 0440 /var/backups/munin.tar
+cd /var/lib/
+rm -vf /var/backups/awt/munin.tar
+tar -cf /var/backups/awt/munin.tar munin
+chmod -v 0440 /var/backups/awt/munin.tar
+ls -l /var/backups/awt/munin.tar
