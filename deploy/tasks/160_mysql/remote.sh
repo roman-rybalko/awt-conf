4491d6c4201c2e062b0c81556857787896dd0799
Roman Rybalko <devel@romanr.info>
2016-01-06 17:43:48 +0300

    deploy: mysql munin

--- deploy/tasks/160_mysql/remote.sh
+++ deploy/tasks/160_mysql/remote.sh
@@ -12,3 +12,34 @@ mysql --user=root --password=`cat pw` --batch < users.sql
 for f in master*.sql; do
 	[ ! -f $f ] || mysql --user=root --password=`cat pw` --batch < $f
 done
+
+apt-get --yes --purge install libcache-cache-perl
+chmod -v 0440 mysql
+chown -v root:munin mysql
+mv -vf mysql /etc/munin/plugin-conf.d/
+for f in \
+bin_relay_log \
+commands \
+connections \
+files_tables \
+innodb_bpool \
+innodb_bpool_act \
+innodb_io \
+innodb_log \
+innodb_rows \
+innodb_semaphores \
+innodb_tnx \
+myisam_indexes \
+network_traffic \
+qcache \
+qcache_mem \
+replication \
+select_types \
+slow \
+sorts \
+table_locks \
+tmp_tables \
+; do
+	ln -svf /usr/share/munin/plugins/mysql_ /etc/munin/plugins/mysql_${f}
+done
+service munin-node restart
--- deploy/tasks/160_mysql/users.sql
+++ deploy/tasks/160_mysql/users.sql
@@ -2,12 +2,15 @@ create user 'awt'@'localhost' identified by 'V0TrpouKDUxUIzb1yubNBW';
 grant all on awt.* to 'awt'@'localhost';
 
 create user 'awt-bak'@'localhost' identified by '9z4NC5cUD68XM7EYHrgKjF';
-grant REPLICATION CLIENT, reload on *.* to 'awt-bak'@'localhost';
-grant select on awt.* to 'awt-bak'@'localhost';
+grant REPLICATION CLIENT, RELOAD on *.* to 'awt-bak'@'localhost';
+grant SELECT on awt.* to 'awt-bak'@'localhost';
 
 create user 'awt-repl'@'%.hosts.advancedwebtesting.net' identified by '7tGN73iGO8yFBCQ3rz2RED';
 grant REPLICATION SLAVE on *.* to 'awt-repl'@'%.hosts.advancedwebtesting.net';
 
 create user 'awt-repl-adm'@'localhost' identified by 'ubEBbVnoy0cCOiW876hNgd';
-grant super, reload on *.* to 'awt-repl-adm'@'localhost';
-grant drop, create, insert on awt.* to 'awt-repl-adm'@'localhost';
+grant SUPER, RELOAD on *.* to 'awt-repl-adm'@'localhost';
+grant DROP, CREATE, INSERT on awt.* to 'awt-repl-adm'@'localhost';
+
+create user 'munin'@'localhost' identified by 'NBS2UpsgfXzqagwIMBQKKZ';
+grant PROCESS, SUPER, REPLICATION CLIENT on *.* to 'munin'@'localhost';
