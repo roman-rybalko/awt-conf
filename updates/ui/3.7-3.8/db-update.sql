c28635e1ecb1b21f10c8765c500811818ac98575
Roman Rybalko <devel@romanr.info>
2016-01-15 17:54:51 +0300

    ui 3.8 update

---
+++ updates/ui/3.7-3.8/db-update.sql
@@ -0,0 +1,5 @@
+alter table test_actions modify selector varchar(8192);
+alter table test_actions modify data varchar(8192);
+alter table task_actions modify selector varchar(8192);
+alter table task_actions modify data varchar(8192);
+alter table history modify data varchar(16384);
