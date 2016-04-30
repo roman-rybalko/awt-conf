e31760c8d3701dc331188277881cc53726fed9f3
Roman Rybalko <devel@romanr.info>
2016-04-30 13:40:35 +0300

    deploy: del squid logs

--- deploy/tasks/100_squid_del/remote.sh
+++ deploy/tasks/100_squid_del/remote.sh
@@ -3,7 +3,7 @@
 
 apt-get --purge --yes remove squid3
 apt-get --purge --yes autoremove
-rm -Rvf /etc/squid3 /var/spool/squid3
+rm -Rvf /etc/squid3 /var/spool/squid3 /var/log/squid3
 
 rm -Rvf /etc/munin/plugin-conf.d/squid /etc/munin/plugins/squid_*
 service munin-node restart
