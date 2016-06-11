d84e6dee665da684c7dacdbbbc035046c2a602e7
Roman Rybalko <devel@romanr.info>
2016-06-11 22:51:38 +0300

    deploy: switch to s2

--- deploy/batch/server_failover.sh
+++ deploy/batch/server_failover.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
 . ./framework.sh
-export IP4=92.63.99.29
-export IP6=2a01:230:2:6::35b
+export IP4=85.143.220.253
+export IP6=2a04:ac00:1:655f:5054:ff:fe00:91ab
 deploy_batch 500_failover_iptables 231_awt_cron_del
