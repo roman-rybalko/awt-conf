fc415d44d6229deb952297b5e0543f0e73911f4a
Roman Rybalko <devel@romanr.info>
2016-06-11 20:34:10 +0300

    deploy: batch server mysql sync w/ docs

--- deploy/batch/mysql.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -ex
-. ./framework.sh
-export ROOT_PW=xxx
-deploy_batch 160_mysql
--- deploy/batch/server_mysql.sh
+++ deploy/batch/server_mysql.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ex
 . ./framework.sh
-export MYSQL_PW=6fEYC1YcZPgeHh9dwJoIdr
+export MYSQL_PW=xxx
 deploy_batch 160_mysql 190_awt_db_backup_del 195_awt_db_backup
