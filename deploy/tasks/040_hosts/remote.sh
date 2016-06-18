06644aa819626f17f50af11f63e78cf1e725141d
Roman Rybalko <devel@romanr.info>
2016-06-18 14:41:37 +0300

    deploy: hosts chattr security

--- deploy/tasks/040_hosts/remote.sh
+++ deploy/tasks/040_hosts/remote.sh
@@ -3,7 +3,9 @@
 cp -vf /etc/hosts /etc/hosts.bak
 grep -v .hosts.advancedwebtesting.net /etc/hosts | grep -v deploy > /etc/hosts.new
 cat hosts >> /etc/hosts.new
+chattr -i /etc/hosts || true
 mv -vf /etc/hosts.new /etc/hosts
+chattr +i /etc/hosts || true
 cat /etc/hosts
 
 service squid3 reload || true
